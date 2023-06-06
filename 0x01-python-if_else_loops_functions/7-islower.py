bea74cb698d9c7058b70629fcec4e4a8e41bd6b6
Abubacer <azizdesignislife@gmail.com>
2023-06-06 23:51:03 +0000

    To uppercase

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,3 +1,11 @@
 #!/usr/bin/python3
+
+""" islower: a function that checks for lowercase character.
+Returns True if c is lowercase or False otherwise."""
+
+
 def islower(c):
-    return 'a' <= c <= 'z'
+    if ord(c) >= 97 and ord(c) <= 122:
+        return True
+    else:
+        return False
---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+"""uppercase: a function that prints a string in uppercase
+followed by a new line"""
+
+
+def uppercase(str):
+    for char in str:
+        if ord(char) >= 97 and ord(char) <= 122:
+            char = chr(ord(char) - 32)
+        print("{}".format(char), end="")
+    print("")
