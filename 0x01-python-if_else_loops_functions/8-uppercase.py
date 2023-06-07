e4f023142c92fc4010a9f2c41d5d4e9e02c8bf4a
Abubacer <azizdesignislife@gmail.com>
2023-06-07 00:51:15 +0000

    Fizz Buzz

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,11 +1,5 @@
 #!/usr/bin/python3
 
-"""
-uppercase - a function that prints a string in uppercase
-followed by a new line.
-"""
-
-
 def uppercase(str):
     for char in str:
         if ord(char) >= 97 and ord(char) <= 122:
