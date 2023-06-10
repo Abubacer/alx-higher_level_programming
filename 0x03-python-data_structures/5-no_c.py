7ab3206ebc934c306945b87c1f8babc1abec0718
Abubacer <azizdesignislife@gmail.com>
2023-06-10 16:15:14 +0000

    Can you C me now?

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,10 +1,7 @@
 #!/usr/bin/env python3
-""" no_c - a function that removes all characters c and C from a string.
-Return the new string.
-"""
-
 
 def no_c(my_string):
+    # a function that removes all characters c and C from a string.
     no_char = "cC"
     chars = [char for char in my_string if char not in no_char]
     new_string = "".join(chars)
