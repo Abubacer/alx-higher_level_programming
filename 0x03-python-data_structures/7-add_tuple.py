a7812d1017d02a3ecd850af4feb06ccfee763f5a
Abubacer <azizdesignislife@gmail.com>
2023-06-10 17:40:06 +0000

    Tuples addition

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
-"""add_tuple - a function that adds 2 tuples.
-Returns a tuple with 2 integers
-"""
-
 
 def add_tuple(tuple_a=(), tuple_b=()):
-    a = tuple_a + (0, 0)[:2 - len(tuple_a)]
-    b = tuple_b + (0, 0)[:2 - len(tuple_b)]
-    first_sum = a[0] + b[0]
-    second_sum = a[1] + b[1]
-    sum_tuple = (first_sum, second_sum)
+    # a function that adds 2 tuples
+    # if a tuple is smaller than 2, use the value 0 for each missing integer
+    # if a tuple is bigger than 2, use only the first 2 integers
+    A = tuple_a + (0, 0)[:2 - len(tuple_a)]
+    # create a new tuple (A) by concatenating tuple_a with a tuple (0, 0).
+    # the slicing operation [:2 - len(tuple_a)] ensure that (A) has length of
+    # at least 2. If tuple_a has a length of 2 or more the slicing is
+    # essentially an empty slice[:] resulting in (A) being the same as tuple_a
+    # otherwise, it appends (0, 0) to tuple_a to ensure (A) has two elements.
+    B = tuple_b + (0, 0)[:2 - len(tuple_b)]
+    sum_tuple = (A[0] + B[0], A[1] + B[1])
     return sum_tuple
