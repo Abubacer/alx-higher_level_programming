e1b12857e8b6d42864c6412b57e96db36fd20713
Abubacer <azizdesignislife@gmail.com>
2023-06-12 18:10:38 +0000

    Linked list palindrome

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,11 +1,8 @@
 #!/usr/bin/python3
-""" element_at - a function that retrieves an element from a list like in C.
-return None if idx is negative or idx is out of range (greater than the number
-of element in my_list).
-"""
-
 
 def element_at(my_list, idx):
+    # a function that retrieves an element from a list.
+    # return None if idx is negative or idx is out of range.
     if idx < 0 or idx >= len(my_list):
         return None
     return my_list[idx]
---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,81 @@
+#include "lists.h"
+
+/**
+* reverse_list - a function that reverses a singly-linked.
+* @head: The starting node of the linked list to reverse.
+*
+* Return: The head of the reversed linked list.
+*/
+
+listint_t *reverse_list(listint_t *head)
+{
+	listint_t *prev_node, *current_node, *next_node;
+
+	prev_node = 0;
+	current_node = head;
+	next_node = 0;
+
+	while (current_node != NULL)
+	{
+		next_node = next_node->next;
+		current_node->next = prev_node;
+		prev_node = current_node;
+		current_node = next_node;
+	}
+
+	return (prev_node);
+}
+
+/**
+ * is_palindrome - a function that that checks if a singly linked list
+ * is a palindrome.
+ * @head: A double pointer to head of linked list.
+ * Return: 1 if the linked list is a palindrome or 0 if it is not.
+ *
+ * Description: the function split the list in the middle into two
+ * lists, reverses the second list and finally compare the splitted
+ * two list and return whether the list is a palindrome or not.
+ */
+
+int is_palindrome(listint_t **head)
+{
+	listint_t *slow_ptr, *fast_ptr, *list_one, *list_two;
+
+	if (*head == NULL || (*head)->next == NULL)
+		return (1);
+
+	slow_ptr = *head;
+	fast_ptr = *head;
+
+	while (slow_ptr)
+	{
+		slow_ptr = slow_ptr->next;
+		if (slow_ptr == NULL)
+		{
+			list_two = fast_ptr->next;
+			break;
+		}
+		if (slow_ptr->next == NULL)
+		{
+			list_two = fast_ptr->next->next;
+			break;
+		}
+		fast_ptr = fast_ptr->next;
+	}
+
+	fast_ptr->next = NULL;
+	list_two = reverse_list(list_two);
+	list_one = *head;
+
+	while (list_one != NULL && list_two != NULL)
+	{
+		if (list_one->n == list_two->n)
+		{
+			list_one = list_one->next;
+			list_two = list_two->next;
+		}
+		else
+			return (0);
+	}
+	return (1);
+}
