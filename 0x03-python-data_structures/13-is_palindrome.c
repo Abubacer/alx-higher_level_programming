c051e07d3b2d9fc837860a1805b8bf14a0bd6a95
Abubacer <azizdesignislife@gmail.com>
2023-06-13 01:13:01 +0000

    update

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -32,9 +32,10 @@ listint_t *reverse_list(listint_t *head)
  * @head: A double pointer to head of linked list.
  * Return: 1 if the linked list is a palindrome or 0 if it is not.
  *
- * Description: the function split the list in the middle into two
- * lists, reverses the second list and finally compare the splitted
- * two list and return whether the list is a palindrome or not.
+ * Description: the function finds the middle of the linked list using
+ * the slow and fast pointers technique, reverses the second half and
+ * finally compare the corresponding nodes in two halves of the list
+ * and return whether the list is a palindrome or not.
  */
 
 int is_palindrome(listint_t **head)
