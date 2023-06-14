04a4743357896c589a1c08aff9a661ae183bfb9b
Abubacer <azizdesignislife@gmail.com>
2023-06-14 19:23:48 +0000

    Squared simple

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def square_matrix_simple(matrix=[]):
+    #  a function that computes the square value of all integers of a matrix.
+    new_matrix = [list(map(lambda n: n ** 2, row)) for row in matrix]
+
+    return new_matrix
+
+    for row in matrix:
+        return (row)
