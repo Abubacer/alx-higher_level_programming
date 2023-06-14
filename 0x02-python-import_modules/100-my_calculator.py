7c3e62abbaacfc94e88999f5cb3b09a9d1a8b749
Abubacer <azizdesignislife@gmail.com>
2023-06-14 01:07:17 +0000

    Build my own calculator!

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,19 +1,21 @@
 #!/usr/bin/python3
-import sys
-from calculator_1 import add, sub, mul, div
+# a program that imports all functions from the file calculator_1.py
+# and handles basic operations.
+if __name__ == "__main__":
+    import sys
+    from calculator_1 import add, sub, mul, div
 
-if len(sys.argv) - 1 != 3:
-    print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-    sys.exit(1)
+    if len(sys.argv) - 1 != 3:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        sys.exit(1)
 
-a = int(sys.argv[1])
-operator = sys.argv[2]
-b = int(sys.argv[3])
+    a = int(sys.argv[1])
+    operator = sys.argv[2]
+    b = int(sys.argv[3])
+    operations = {"+": add, "-": sub, "*": mul, "/": div}
 
-operations = {"+": add, "-": sub, "*": mul, "/": div}
+    if operator not in operations:
+        print("Unknown operator. Available operators: +, -, * and /")
+        sys.exit(1)
 
-if operator not in operations:
-    print("Unknown operator. Available operators: +, -, * and /")
-    sys.exit(1)
-
-print("{} {} {} = {}".format(a, operator, b, operations[operator](a, b)))
+    print("{} {} {} = {}".format(a, operator, b, operations[operator](a, b)))
