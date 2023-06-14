b5a1159539ae4655a857c8bbf58a7d59ab0a189c
Abubacer <azizdesignislife@gmail.com>
2023-06-14 20:15:27 +0000

    Present in both

---
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+
+def common_elements(set_1, set_2):
+    # a function that returns a set of common elements in two sets.
+    common_elemt = set_1.intersection(set_2)
+    return common_elemt
