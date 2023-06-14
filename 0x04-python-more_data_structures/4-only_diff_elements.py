a4034d40925e9ddee3013acd6ead886107f04632
Abubacer <azizdesignislife@gmail.com>
2023-06-14 20:36:42 +0000

    Only differents

---
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+
+def only_diff_elements(set_1, set_2):
+    # a function that returns a set of all elements present in only one set.
+    diff_set = set_1.union(set_2).difference(set_1.intersection(set_2))
+    return diff_set
