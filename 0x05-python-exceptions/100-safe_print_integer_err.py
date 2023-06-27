1c7ad1a7e2b5fc3d3573ab5f2adf566691e8782e
Abubacer <azizdesignislife@gmail.com>
2023-06-27 00:37:57 +0000

    Safe integer print with error message

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 import sys
 
+
 def safe_print_integer_err(value):
     # a function that prints an integer with error message.
     # Returns True if value  is an integer, Otherwise, returns False.
