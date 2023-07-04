228143944efe90dcb92afd9b10a8b2624243dc88
Abubacer <azizdesignislife@gmail.com>
2023-07-04 01:00:51 +0000

    Real definition of a rectangle

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -66,11 +66,3 @@ class Rectangle:
         elif value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
-
-    def __repr__(self):
-        """ Returns the string representation of the rectangle.
-
-        Returns:
-            str: The string representation.
-        """
-        pass
--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -68,13 +68,11 @@ class Rectangle:
         self.__height = value
 
     def area(self):
-        """ Returns the rectangle area.
-        """
+        """ Returns the rectangle area."""
         return (self.__width * self.__height)
 
     def perimeter(self):
-        """ Returns the rectangle perimeter.
-        """
+        """ Returns the rectangle perimeter."""
         if self.__width == 0 or self__.height == 0:
             return 0
-        return ((self.__width * 2) + (self.__height * 2)) 
+        return ((self.__width * 2) + (self.__height * 2))
