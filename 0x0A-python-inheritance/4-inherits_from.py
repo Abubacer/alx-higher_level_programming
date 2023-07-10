aa85a41ea5d6c266b75dba6a7934344561e36863
Abubacer <azizdesignislife@gmail.com>
2023-07-10 19:54:44 +0000

    Only sub class of

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -2,9 +2,9 @@
 """Checks the object instance"""
 
 
-def is_kind_of_class(obj, a_class):
-    """Checks if the object is an instance of, or if the object is an
-    instance of a class that inherited from, the specified class.
+def inherits_from(obj, a_class):
+    """Checks if the object is an instance of a class that inherited
+    (directly or indirectly) from the specified class.
 
     Args:
         obj (any): the object to check.
@@ -14,7 +14,7 @@ def is_kind_of_class(obj, a_class):
          True if the object is an instance of the class.
          False otherwise.
     """
-    if isinstance(obj, a_class):
+    if issubclass(type(obj), a_class) and type(obj) != a_class:
         return True
     else:
         return False
