c3489e1b5ba0879ad437f51fe2dfe87555170602
Abubacer <azizdesignislife@gmail.com>
2023-07-12 00:08:14 +0000

    Student to disk and reload

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+"""Defines a student class."""
+
+
+class Student:
+    """
+    This class represents a Student.
+    """
+    def __init__(self, first_name, last_name, age):
+        """
+        Initializes a Student.
+
+        Args:
+            first_name (str): the first name.
+            last_name (str): the last name.
+            age (int): the age of the Student.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        Retrieves a dictionary representation of a Student instance.
+        If attrs is a list of strings, only attribute names contained
+        in this list must be retrieved.
+        Otherwise, all attributes must be retrieved.
+
+        Args:
+            attrs: optional list of attribute names to be retrieved.
+        """
+        jsonDict = {}
+
+        obj_attr = vars(self)
+
+        for attr, value in obj_attr.items():
+            if attrs is None or attr in attrs:
+                if isinstance(value, (list, dict, str, int, bool)):
+                    jsonDict[attr] = value
+
+        return jsonDict
+
+    def reload_from_json(self, json):
+        """
+        Replaces all attributes of the Student instance with new value.
+
+        Args:
+            json: a dictionary containing the new attribute.
+        """
+        for attr, value in json.items():
+            setattr(self, attr, value)
