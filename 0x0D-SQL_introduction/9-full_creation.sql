81acddab1d7f391101784cc083d3fb8521d1e330
Abubacer <azizdesignislife@gmail.com>
2023-08-15 18:56:28 +0000

    fix task Full creation

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,6 +1,6 @@
 -- Creates a table second_table in the database hbtn_0c_0 in MySQL server and add multiples rows.
 -- second_table description: id INT, name VARCHAR(256), score INT.
-CREATE TABLE second_table (
+CREATE TABLE IF NOT EXISTS second_table (
    id INT,
    name VARCHAR(256),
    score INT
