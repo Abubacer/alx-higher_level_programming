b3fbe7dd6519dd2bde17f91ce62e92759c91dab5
Abubacer <azizdesignislife@gmail.com>
2023-08-16 17:10:33 +0000

    User

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,3 @@
+-- Creates the MySQL server user user_0d_1, with all privileges on your MySQL server.
+-- The user_0d_1 password is set to user_0d_1_pwd.
+CREATE USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
