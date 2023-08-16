614e7cd4c1f1c1b81dc295bc11fd756dfdfadc68
Abubacer <azizdesignislife@gmail.com>
2023-08-16 22:04:59 +0000

    Cities of California

---
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -0,0 +1,6 @@
+-- Creates the table force_name on my MySQL server.
+-- Description: id INT, name VARCHAR(256) can’t be null.
+CREATE TABLE IF NOT EXISTS force_name (
+   id INT,
+   name VARCHAR(256) NOT NULL
+);
