465f59964c0179165b5d892cf806d5fb8ab8303a
Abubacer <azizdesignislife@gmail.com>
2023-09-13 18:58:48 +0000

    fix seconde biggest

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 // A script that searches the second biggest integer in the list of arguments.
-
 const argv = process.argv;
 if (argv.length <= 3) {
   console.log(0);
 } else {
-  const argsList = argv.sort();
-  console.log(argsList.reverse()[1]);
+  const argsToNumb = argv.slice(2).map(Number);
+  const sortedList = argsToNumb.sort((x, y) => y - x);
+  console.log(sortedList[1]);
 }
