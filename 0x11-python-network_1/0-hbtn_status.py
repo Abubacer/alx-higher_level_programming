99af6d0483e6d3f42814f7373e37ae3dc0d5ee25
Abubacer <azizdesignislife@gmail.com>
2023-09-29 16:48:28 +0000

    Fetch an url using urllib

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -14,6 +14,6 @@ if __name__ == "__main__":
         content = response.read()
 
         print("Body response:")
-        print("\t- type: {}".format(type(content)))
-        print("\t- content: {}".format(content))
-        print("\t- utf8 content: {}".format(content.decode('utf-8')))
+        print("	- type: {}".format(type(content)))
+        print("	- content: {}".format(content))
+        print("	- utf8 content: {}".format(content.decode('utf-8')))
