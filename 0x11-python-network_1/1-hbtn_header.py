6f1fd1997b6ec6591849be738cad7d8d63e77e52
Abubacer <azizdesignislife@gmail.com>
2023-09-29 17:05:22 +0000

    Response header value

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Takes in a URL, sends a request to the URL and displays the value of a variable
+found in the header of the response.
+"""
+
+import sys
+from urllib import request
+
+if __name__ == "__main__":
+
+    url = sys.argv[1]
+
+    with request.urlopen(url) as response:
+        content = response.getheader('X-Request-Id')
+
+        print("{}".format(content))
