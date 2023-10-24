5db118bb7108f976ef7a6d96f44a30c989d199a9
Abubacer <azizdesignislife@gmail.com>
2023-10-24 18:22:42 +0000

    writes a string to a file

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+// Reads and prints the content of a file in utf-8.
+const fs = require('fs');
+// The first argument is the file path
+const filePath = process.argv[2];
+// The second argument is the string to write
+const stringInput = process.argv[3];
+// Reads and prints the content of a file
+fs.writeFile(filePath, stringInput, 'utf8', (err) => {
+  if (err) {
+    console.error(err);
+  } else {
+    console.log(`${stringInput}`);
+  }
+});
