d33198e92533b507948645f8c398eb9bd3527175
Abubacer <azizdesignislife@gmail.com>
2023-10-24 20:04:14 +0000

    computes the number of tasks completed by user id

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,25 @@
+#!/usr/bin/node
+// Prints and computes the number of tasks completed by user id.
+const request = require('request');
+// The first argument is the API URL
+const apiUrl = process.argv[2];
+// Fetchs todos data by user
+request(apiUrl, (err, response, body) => {
+  if (err) {
+    console.error(err);
+  } else {
+    const todosData = JSON.parse(body);
+    const completedTasks = {};
+
+    todosData.forEach((task) => {
+      if (completedTasks) {
+        if (completedTasks[task.userId]) {
+          completedTasks[task.userId]++;
+        } else {
+          completedTasks[task.userId] = 1;
+        }
+      }
+    });
+    console.log(completedTasks);
+  }
+});
